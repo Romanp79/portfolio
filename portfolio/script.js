bbc3c5e897a0899ee9e7e1ced115dae97f5632ed
Romanp79 <150139787+Romanp79@users.noreply.github.com>
2026-03-26 02:01:58 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,9 +0,0 @@
-function showSection(id){
-    document.querySelectorAll("section").forEach(sec=>{
-        sec.classList.remove("active");
-    });
-
-    setTimeout(()=>{
-        document.getElementById(id).classList.add("active");
-    },100);
-}
