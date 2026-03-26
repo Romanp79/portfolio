b3f5090edace26b1aeda9101de23fb737c199118
Romanp79 <150139787+Romanp79@users.noreply.github.com>
2026-03-27 03:29:52 +0530

    Refactor section display and add loader on page load
    Refactor showSection function and add loader functionality.

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+// LOADER
+window.onload = function(){
+    document.getElementById("loader").style.display="none";
+
+    // show animation for first section
+    activateAnimations(document.querySelector(".active"));
+}
+
+// FUNCTION TO ACTIVATE ANIMATION
+function activateAnimations(section){
+    const elements = section.querySelectorAll(".animate");
+
+    elements.forEach((el, index)=>{
+        setTimeout(()=>{
+            el.classList.add("show");
+        }, index * 150);
+    });
+}
+
+// SECTION SWITCH
 function showSection(id){
     document.querySelectorAll("section").forEach(sec=>{
         sec.classList.remove("active");
     });
 
     const section = document.getElementById(id);
+    section.classList.add("active");
 
-    setTimeout(()=>{
-        section.classList.add("active");
+    // reset animations
+    section.querySelectorAll(".animate").forEach(el=>{
+        el.classList.remove("show");
+    });
 
-        const elements = section.querySelectorAll(".animate");
-
-        elements.forEach((el, index)=>{
-            setTimeout(()=>{
-                el.classList.add("show");
-            }, index * 150);
-        });
-
-    },100);
+    activateAnimations(section);
 }
+
+// CURSOR
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", e=>{
+    cursor.style.top = e.clientY + "px";
+    cursor.style.left = e.clientX + "px";
+});
